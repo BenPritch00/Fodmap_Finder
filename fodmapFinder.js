438892fd543f6e8de1ec01f14e1f1474cfedcc36
Ben Pritchard <benpritch00@Bens-MacBook-Air.local>
2021-08-16 18:18:35 -0400

    basic logic to check recipe for fodmaps

---
+++ fodmapFinder.js
@@ -0,0 +1,15 @@
+// write a function, that given a list of fodmaps and a string containing a list of ingredients seperated by a comma, determines whether the recipe has any high fodmap ingredients
+const sampleRecipe = "garlic, potatoes, cherries, butter" 
+
+const sampleFodmaps = ["garlic", "onion", "milk"]
+function recipeChecker(recipe, fodmaps) {
+    // convert our recipe to a list of words
+    // Check each ingredient in our recipe to see if it's a fodmap
+    const recipeArr = recipe.split(',');
+    for (let i = 0; i < recipeArr.length; i++) {
+        if (fodmaps.includes(recipeArr[i])) {
+            console.log(recipeArr[i]);
+          }
+    }
+}
+recipeChecker(sampleRecipe, sampleFodmaps)
